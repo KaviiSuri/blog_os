e325d8850e7ec3bc27ef5bdc44531aee829f4928
KaviiSuri <kavii@speechify.com>
2023-10-21 05:28:26 +0530

    added exception handlers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
+use blog_os::println;
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    panic!("Some panic message");
+    blog_os::init();
+
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
     loop {}
 }
 
-/// This function is called on panic.
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
